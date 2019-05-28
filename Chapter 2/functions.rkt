9c73492238b6fb473e758c867b0e69dcffd5b364
Kirk Thaker <kirkthaker66@gmail.com>
2019-05-28 13:58:58 -0400

    Wrote a function to refresh memory about Racket

--- Chapter 2/functions.rkt
+++ Chapter 2/functions.rkt
@@ -17,3 +17,15 @@
       [else "Tie game."])))
 
 (play-game)
+
+; A function that checks if string equals "password" (case insensitive)
+(define (check-pass p)
+  (if (string? p)
+      (if (equal? (string-downcase p) "password")
+          "Correct password"
+          "Incorrect password")
+      "Invalid input: not a string"))
+
+(check-pass 123)
+(check-pass "pass")
+(check-pass "password")
